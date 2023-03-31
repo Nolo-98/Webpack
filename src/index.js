8c12bd6ceb406e19bdd5fb8e5461f05d9956db4c
Nolo-98 <twisterfast@gmail.com>
2023-03-30 22:09:25 -0600

    Proyecto a empaquetar

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const $links = document.getElementById('links');
+const $name = document.querySelector('h1');
+
+const data = {
+    name: "David Ortiz",
+    nickname: "Nolo98",
+    description: "...",
+    avatar: "...",
+    social: [
+      {
+        name: "faceebok",
+        url: "https://twitter.com/",
+        username: "",
+      },
+      {
+        name: "instagram",
+        url: "https://instagram.com/",
+        username: "",
+      },
+    ],
+    links: [
+      {
+        name: "blog",
+        url: "https://example.com/",
+        color: "red",
+        emoji: "📖",
+      },
+      {
+        name: "podcast",
+        url: "https://example.com/",
+        color: "yellow",
+        emoji: "💬",
+      },
+    ],
+    footer: "Made with Love on Mexico-DF",
+  };
